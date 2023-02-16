59926df8e150927665d0c52545823c13b23df749
anna-arlig <anna.arlig@iths.se>
2023-02-16 15:06:27 +0100

    Initial commit

---
+++ src/constants/colors.ts
@@ -0,0 +1,11 @@
+export default {
+
+  primary: {
+    background_green: '#F3FAFB',
+    medium_green: '#D7E2E3',
+    dark_green: '#004A51',
+    dark_green_opacity: 'rgba(0, 74, 81, 0.8)',
+    white: '#FFFFFF',
+    error_red: '#CB1515'
+  },
+  }
---
+++ src/views/styled.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+import colors from '../constants/colors';
+
+export const Logo = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: ${colors.primary.dark_green};
+`;
