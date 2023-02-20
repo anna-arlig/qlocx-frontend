26c7a5137ac7569217c9864ac65ca2d25761537b
anna-arlig <anna.arlig@iths.se>
2023-02-20 13:22:40 +0100

    Add axios and login action

--- src/actions/login.js
+++ src/actions/login.js
@@ -1,17 +1,36 @@
 import { redirect } from "react-router-dom"
+import axios from "axios"
+
+axios.defaults.baseURL = 'http://localhost:3030/api/user'
+
+
 
 export const loginAction = async ({request}) => {
 
   const data = await request.formData()
 
-  const formData = {
-    email: data.get('email'), 
-    password: data.get('password')
+  const email = data.get('email')
+  const password = data.get('password')
+
+  try{
+    const response = await axios
+      .post("/auth/", {
+        email,
+        password,
+      })
+    const token = response.data.token
+    axios.defaults.headers.common["Authorization"] = `Bearer ${token}`
+    const userInfo = await axios.get('/find/1337')
+    console.log(userInfo)
+    return redirect('user')
+  }
+  catch { 
+    console.log('403 - Unauthorized')
   }
 
 
 
-  return redirect('user')
+  return redirect('/')
 
 
 }
