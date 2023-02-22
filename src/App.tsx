cbf8eab06a203747e7db554f816a8041b6aa033e
anna-arlig <anna.arlig@iths.se>
2023-02-22 10:37:51 +0100

    Remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import Header from './components/Header/Header'
 import GlobalStyle from './GlobalStyle'
-import { BrowserRouter, Routes } from 'react-router-dom'
+
 const App = () => {
   return (
     <>
